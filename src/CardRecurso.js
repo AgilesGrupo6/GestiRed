6275a979f1892bc19a88e2a6dc972a895aeb5cb7
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-10-10 02:29:05 -0500

    consultar usuarios participantes de recurso OK

--- src/CardRecurso.js
+++ src/CardRecurso.js
@@ -7,18 +7,33 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import IconButton from '@material-ui/core/IconButton';
+import PeopleIcon from '@material-ui/icons/People';
 
-
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+  },
+});
 
 export default class CardRecurso extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      icon:'/movies.ico'
+      icon:'/movies.ico',
+      open:false
     };
 
     this.selectIcon = this.selectIcon.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   selectIcon(){
@@ -36,6 +51,25 @@ export default class CardRecurso extends Component {
     }
   }
 
+  handleOpen(){
+    this.setState({ open: true });
+  }
+
+  handleClose(){
+    this.setState({ open: false });
+  }
+
+  getModalStyle() {
+    const top = 50 + Math.round(Math.random() * 20) - 10;
+    const left = 50 + Math.round(Math.random() * 20) - 10;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
   render() {
     return (
       <div>
@@ -51,9 +85,32 @@ export default class CardRecurso extends Component {
             <p>{this.props.recurso.tipoRecurso}</p>
           </CardContent>
           <CardActions className='actions'>
+            <IconButton
+              onClick={this.handleOpen}
+              aria-label="Ver participantes"
+            >
+              <PeopleIcon />
+            </IconButton>
             <Button href={this.props.recurso.url}>Ver recurso</Button>
           </CardActions>
         </Card>
+        <Modal
+          aria-labelledby='simple-modal-title'
+          aria-describedby="simple-modal-description"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <div style={this.getModalStyle()} className='paper'>
+            <Typography variant="headline" id="modal-title">
+              {'Participantes en: ' +this.props.recurso.nombre}
+            </Typography>
+            {this.props.recurso.responsables.map(r => {
+              return (<Typography key={r} variant="subheading" id="simple-modal-description">
+                {r}
+              </Typography>);
+            })}
+          </div>
+        </Modal>
       </div>
     );
   }
